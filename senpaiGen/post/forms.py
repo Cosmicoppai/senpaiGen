83b9c71fb2548daca02fcfa21949562454473698
Cosmicoppai <kanakchaudhari12@gmail.com>
2021-06-03 16:58:07 +0530

    "Added the add post feature"
    comment via ajax,
    improve edit profile page,
    Follow users ,

--- senpaiGen/post/forms.py
+++ senpaiGen/post/forms.py
@@ -8,7 +8,7 @@ class AddPostForm(forms.ModelForm):
         fields = ('title', 'body', 'image')
 
         widgets = {
-            'title': forms.TextInput(attrs={'class':'form-control','id':"title-name",'placeholder':"Title" }),
+            'title': forms.TextInput(attrs={'class':'form-control','id':"title-name",'placeholder':"Title"}),
             'body': forms.Textarea(attrs={'class':'form-control','id':"body-text",'placeholder':"Body/Text", 'rows':5}),
             'image': forms.FileInput(attrs={'class':'form-control', 'id':"image", 'accept':'image/*', 'placeholder':"Image"}),
         }
