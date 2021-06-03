911e82d07175716670a64e1e43344999398880bb
Cosmicoppai <kanakchaudhari12@gmail.com>
2021-06-03 17:23:33 +0530

    "updated post/forms.py"
    comment via ajax,
    improve edit profile page,
    Follow users ,

--- senpaiGen/post/forms.py
+++ senpaiGen/post/forms.py
@@ -1,5 +1,6 @@
 from .models import Post
 from django import forms
+from django.utils.translation import gettext as _
 
 
 class AddPostForm(forms.ModelForm):
@@ -8,7 +9,33 @@ class AddPostForm(forms.ModelForm):
         fields = ('title', 'body', 'image')
 
         widgets = {
-            'title': forms.TextInput(attrs={'class':'form-control','id':"title-name",'placeholder':"Title"}),
-            'body': forms.Textarea(attrs={'class':'form-control','id':"body-text",'placeholder':"Body/Text", 'rows':5}),
-            'image': forms.FileInput(attrs={'class':'form-control', 'id':"image", 'accept':'image/*', 'placeholder':"Image"}),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'id': "title-name", 'placeholder': _("Title")}),
+            'body': forms.Textarea(
+                attrs={'class': 'form-control', 'id': "body-text", 'placeholder': _("Body"), 'rows': 5}),
+            'image': forms.FileInput(
+                attrs={'class': 'form-control', 'id': "image", 'accept': 'image/*', 'placeholder': _("Image")}),
+        }
+
+        labels = {
+            'title': '',
+            'body': '',
+            'image': '',
+        }
+
+        help_texts = {
+            'title': _('Enter the Title of the Post'),
+            'body': _('Describe your Question Properly'),
+            'image': _('Only "jpg, png, jpeg" formats are allowed')
+        }
+
+        error_messages = {
+            'title': {
+                'max_length': _('Title should be of length less than or equal to 100 characters'),
+            },
+            'body': {
+                'max_length': _('Body should be of length less than or equal to 10000 characters'),
+            },
+            'image':{
+                'validation': _('Enter a Valid Image Format (jpg, png, jpeg)'),
+            }
         }
